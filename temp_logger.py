0c6313e8a04da4545c5a69e61f51c96e7fd5e6cd
Gabriel Meringolo <gabriel.meringolo@gmail.com>
2016-06-23 22:08:59 -0700

    cleaned up and added comments

--- temp_logger.py
+++ temp_logger.py
@@ -5,6 +5,8 @@ from datetime import datetime
 os.system('modprobe w1-gpio')
 os.system('modprobe w1-therm')
 
+
+''' Six temp sensors hooked up to raspi, change temp_sensor_x[0] to change name'''
 temp_sensor_1 = ['Sensor 1', '/sys/bus/w1/devices/28-0000072a6c7a/w1_slave']
 temp_sensor_2 = ['Sensor 2', '/sys/bus/w1/devices/28-0115915062ff/w1_slave']
 temp_sensor_3 = ['Sensor 3', '/sys/bus/w1/devices/28-041591204dff/w1_slave']
@@ -20,32 +22,14 @@ all_sensors = [temp_sensor_1,
                temp_sensor_6]
 
 
-#def temp_raw():
-#    f = open(temp_sensor_1)
-#    lines = f.readlines()
-#    f.close()
-#    return lines
-
-#def read_temp():
-#    for temp_sensor in all_sensors:
-#        with open(temp_sensor[1]) as raw_file:
-#            lines = raw_file.readlines()
-#            while lines[0].strip()[-3:] != 'YES':
-#                sleep(0.2)
-#                lines = raw_file.readlines()
-#            temp_output = lines[1].find('t=')
-#            if temp_output != -1:
-#                temp_string = lines[1].strip()[temp_output+2:]
-#                temp_c = float(temp_string) / 1000.0
-#                temp_f = temp_c * 9.0 /5.0 + 32.0
-#            print(temp_sensor[0], temp_f, datetime.now().strftime('%m/%d/%Y, %I:%M %p'))  
-
-
 def temp_writer(data_to_write):
+    ''' writes data to daily txt file log '''
     with open('{}.txt'.format(datetime.now().strftime('%m.%d.%Y')), 'a+') as f:
         f.write(data_to_write)
 
+
 def read_temp():
+    ''' reads and converts temp from sensor, logs if disconected '''
     for temp_sensor in all_sensors:
         if os.path.exists(temp_sensor[1]):
             try:
@@ -79,7 +63,7 @@ def read_temp():
 
 while True:
     read_temp()
-    sleep(5)
+    sleep(5)  # change sleep time to change cycle...
 
 
 
