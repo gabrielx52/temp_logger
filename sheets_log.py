675f09c3be462fe9917f1a4f8211773e9a7e53b7
Gabriel Meringolo <gabriel.meringolo@gmail.com>
2016-07-03 21:00:43 -0700

    added error logging and moved credentials

--- sheets_log.py
+++ sheets_log.py
@@ -6,13 +6,10 @@ from oauth2client.service_account import ServiceAccountCredentials
 
 from temp_logger import temp_row_maker 
 
-scope = ['https://spreadsheets.google.com/feeds']
-
-credentials = ServiceAccountCredentials.from_json_keyfile_name('/home/pi/temp_logger/palace temp logger-88911bdd605e.json', scope)
-
-gc = gspread.authorize(credentials)
-
-sh = gc.open("palace kitchen temp log")
+# scope = ['https://spreadsheets.google.com/feeds']
+# credentials = ServiceAccountCredentials.from_json_keyfile_name('/home/pi/temp_logger/palace temp logger-88911bdd605e.json', scope)
+# gc = gspread.authorize(credentials)
+# sh = gc.open("palace kitchen temp log")
 
 #wks = gc.open("palace kitchen temp log").sheet1
 #wks.update_acell('A1', 'test')
@@ -21,6 +18,10 @@ sh = gc.open("palace kitchen temp log")
 #print(wks)
 
 def sheets_logger():
+    scope = ['https://spreadsheets.google.com/feeds']
+    credentials = ServiceAccountCredentials.from_json_keyfile_name('/home/pi/temp_logger/palace temp logger-88911bdd605e.json', scope)
+    gc = gspread.authorize(credentials)
+    sh = gc.open("palace kitchen temp log")
     try:
         worksheet = sh.worksheet(datetime.now().strftime('%m.%d.%Y'))
         worksheet.append_row(temp_row_maker())
@@ -36,8 +37,11 @@ def sheets_logger():
                               'sensor 5',
                               'sensor 6'])
         worksheet.append_row(temp_row_maker())
+    except Exception as e:
+        with open('error_log.txt','a+') as er_log:
+            er_log.write('ERROR: {} at {:.2f}\u00b0F \n'.format(str(e), datetime.now().strftime('%m/%d/%Y, %I:%M %p')))
 
 
 while True:
     sheets_logger()
-    sleep(15)
+    sleep(60)
